71cfe09fb6eafe8ceaeb8bece1dd935d5e2d3227
Craimi <jesusmedina.jmh@gmail.com>
2024-05-31 14:12:07 -0600

    Update agregar_dieta.blade.php
    Se eliminó un label (Vista de agregar_dieta) que contenia la frecuencia de consumo de algunos alimentos, debido a que esto ya va en la vista de Nota

--- resources/views/alumno/agregar_dieta.blade.php
+++ resources/views/alumno/agregar_dieta.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,71 +8,68 @@
     <body>
         <h1>El un formulario para registrar pacientes</h1>
         
-        <form action="">
-            <label>
-                Titulo:
-                <input type="text" name="title">
-            </label>
-            <br>
+        <form action="crear" method="POST">
+            @csrf
+            
             <label>
                 Tipo de instrumentos:
-                <input type="radio" name="instrumentos" id="hrs">24 hrs <input type="radio" name="instrumentos" id="semi">Semicuantitativa 
-                <input type="radio" name="instrumentos" id="diario">Diario
+                <input type="radio" name="instrumento" id="hrs">24 hrs
+                <input type="radio" name="instrumento" id="semi">Semicuantitativa 
+                <input type="radio" name="instrumento" id="diario">Diario
             </label>
             <br>
             <label>
-                Desayuno hora:
-                <input type="text" name="desayuno-h">
+                Desayuno hora: 
+                <input type="text" name="desayuno_hora">
             </label>
             <br>
             <label>
                 Colación:
-                <input type="text" name="desayuno-c">
+                <input type="text" name="colacion1">
             </label>
             <br>
             <label>
                 Comida hora:
-                <input type="text" name="comida-h">
+                <input type="text" name="comida_hora">
             </label>
             <br>
             <label>
                 Colación:
-                <input type="text" name="comida-c">
+                <input type="text" name="colacion2">
             </label>
             <br>
             <label>
                 Cena hora:
-                <input type="text" name="cena-h">
+                <input type="text" name="cena_hora">
             </label>
             <br>
             <label>
                 Colación:
-                <input type="text" name="cena-c">
-            </label>
-            <br><br>
-            <label>
-                Total EQ: <br>
-                Verduras:<input type="text" name="verduras">Frutas:<input type="text" name="frutas">Cereales:<input type="text" name="cereales">
-                Leguminosas:<input type="text" name="leguminosas">Carnes:<input type="text" name="carnes">Leche:<input type="text" name="leche">
-                Grasa:<input type="text" name="grasa">Azúcar:<input type="text" name="azucar">
+                <input type="text" name="colacion3">
             </label>
             <br><br>
             <label>
                 TOTAL:<br>
-                Kcal:<input type="text" name="kcal">Prot:<input type="text" name="prot-per">(<input type="text" name="prot-g">) 
-                Lip:<input type="text" name="lip-per">(<input type="text" name="lip-g">) Hco:<input type="text" name="hco-per">(<input type="text" name="hco-g">)
+                Kcal:<input type="text" name="total_kcal">
+                Prot:<input type="text" name="total_prot">(<input type="text" name="prot-g">) 
+                Lip:<input type="text" name="total_lip">(<input type="text" name="lip-g">) 
+                Hco:<input type="text" name="total_hco">(<input type="text" name="hco-g">)
             </label>
             <br><br>
             <label>
                 % ADECUACIÓN:<br>
-                Energia:<input type="text" name="adec-energ"> Kcal:<input type="text" name="adec-kcal"> Prot:<input type="text" name="adec-prot"> 
-                Lip:<input type="text" name="adec-lip"> Hco:<input type="text" name="adec-hco"><br>
-                Aspectos cualitativos de dieta habitual:<input type="text" name="adec-habitual">
+                Energia:<input type="text" name="adecuacion_porcen_ene">
+                Kcal:<input type="text" name="adecuacion_porcen_ener_kcal">
+                Prot:<input type="text" name="adecuacion_porcen_prot"> 
+                Lip:<input type="text" name="adecuacion_porcen_lip">
+                Hco:<input type="text" name="adecuacion_porcen_hco"><br>
+                Aspectos cualitativos de dieta habitual:<input type="text" name="aspectos_cualita_dieta_habitual">
             </label>
             <br><br>
             <label>
                 REQUERIMIENTOS:<br>
-                Energia:<input type="text" name="req-energ"> Proteina total:<input type="text" name="req-prot-g">(<input type="text" name="req-prot-dia">)
+                Energia:<input type="text" name="req-energ">
+                Proteina total:<input type="text" name="req-prot-g">(<input type="text" name="req-prot-dia">)
             </label>
             <br><br>
             <label>
@@ -87,8 +84,10 @@
             <br>
             <label>
                 PLAN DE ALIMENTACIÓN:<br>
-                Dieta <input type="text" name="plan-dieta"> de <input type="text" name="plan-de"> Prot:<input type="text" name="plan-prot-per">(<input type="text" name="plan-prot-g">)
-                Lip:<input type="text" name="plan-lip-per">(<input type="text" name="plan-lip-g">) Hco:<input type="text" name="plan-hco-per">(<input type="text" name="plan-hco-g">)
+                Dieta <input type="text" name="plan-dieta"> de <input type="text" name="plan-de">
+                Prot:<input type="text" name="plan-prot-per">(<input type="text" name="plan-prot-g">)
+                Lip:<input type="text" name="plan-lip-per">(<input type="text" name="plan-lip-g">)
+                Hco:<input type="text" name="plan-hco-per">(<input type="text" name="plan-hco-g">)
             </label>
             <label>
                 SUPLEMENTOS:<br>
@@ -132,6 +131,10 @@
                 NOMBRE COMPLETO, FIRMA Y CÉDULA PROFESIONAL DE NUTRIÓLOG() RESPONSABLE:<br>
                 <input type="text" name="datos-nutriologo">
             </label>
+            
+            <button type="submit">
+                Crear
+            </button>
         </form>
     </body>
 </html>
