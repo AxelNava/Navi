0dc0635508c0954ebbae06710e345217cad28ba8
Sistemas Vadai <diegoldeve@gmail.com>
2024-06-09 22:00:22 -0600

    vista de pacientes de alumnos en director

--- resources/views/director/lista_pacientes_alumno.blade.php
+++ resources/views/director/lista_pacientes_alumno.blade.php
@@ -13,6 +13,18 @@
             margin: 4px 2px;
             cursor: pointer;
         }
+				.pacientes{
+					display: flex;
+					flex-wrap: wrap;
+					/* border: #618ef2 1px solid; */
+				}
+				.paciente{
+					width: 30%;
+					margin: 10px;
+					padding: 10px;
+					border: #618ef2 1px solid;
+					border-radius: 10px;
+				}
     </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -20,30 +32,45 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h1 class="nutriologo-nombre"></h1>
-										<button type="button" style="display:none" id='trigger' data-idNutriologo="{{ $id_nutriologo }}"></button>                    <div id="pacientes">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="py-12">
+	<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+		<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+			<div class="p-6 text-gray-900">
+				<h1 class="nutriologo-nombre"></h1>
+				<h1 class="nutriologo-pacientes"></h1>
+				<button type="button" style="display:none" id='trigger' data-idNutriologo="{{ $id_nutriologo }}"></button>                    <div id="pacientes">
+				<div class="pacientes">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </x-app-layout>
 <script>
 document.addEventListener('DOMContentLoaded', () => {
 	let idNutriologo = {!! json_encode($id_nutriologo) !!};
 	let nombreNutriologo = document.querySelector('.nutriologo-nombre');
+	let pacientesNutriologo = document.querySelector('.nutriologo-pacientes');
+	let pacientes = [];
 	let trigger = document.getElementById('trigger');
-
+	let pacientesContainer = document.querySelector('.pacientes');
 	trigger.addEventListener('click', () => {
 		fetch(`/director/lista_pacientes_alumno_data/${idNutriologo}`)
 			.then(response => response.json())
 			.then(data => {
-				console.log(data);
+				let datosPacientes = data['Datos de sus pacientes'];
 				nombreNutriologo.innerHTML = `ESTUDIANTE: ${data['Datos del alumno'].nombre}`;
+				pacientesNutriologo.innerHTML = `PACIENTES: ${data['Cantidad de pacientes del alumno']}`;
+				datosPacientes.map(paciente => {
+					let html = `
+						<div class="paciente">
+							<b style="font-size:20px;font-weight:900">${paciente.nombre}</b>
+							<p>Género: ${paciente.genero}</p>
+							<p>Edad:${paciente.edad}</p>
+						</div>
+					`;
+					pacientesContainer.innerHTML += html;
+				});
 			});
 	});
 
